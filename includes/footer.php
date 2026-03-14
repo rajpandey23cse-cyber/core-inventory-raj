ab0eeea0c7e1101592472a832e26040606db39e6
rajpandey23cse-cyber <rajpandey.23.cse@iite.indusuni.ac.in>
2026-03-14 17:18:54 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+</main><!-- end main-content -->
+</div><!-- end main-wrapper -->
+<script src="/inventory/assets/js/main.js"></script>
+<?php if (isset($extraJS)): ?>
+<script src="<?= $extraJS ?>"></script>
+<?php endif; ?>
+</body>
+</html>
